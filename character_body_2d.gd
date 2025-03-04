77809375c4842d5129fdffd6ffcf4916395b4e93
Pete01723 <gamerstagged@yahoo.com>
2025-03-04 21:39:09 +0800

    added left and right movement

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -10,6 +10,7 @@ var hooked= false;
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
+const GRAVITY = 980
 
 func Hook_Process(delta: float) -> void:
 	if Input.is_action_just_pressed("left_click") and not hooked:
@@ -41,21 +42,16 @@ func Hook_Process(delta: float) -> void:
 	var grounded = get_slide_collision_count()>0
 
 func _physics_process(delta):
+	var direction = Input.get_axis("move_left", "move_right")
 	Hook_Process(delta)
-	# Add the gravity.
+	
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += GRAVITY * delta
+	
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * delta)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0.0, SPEED * delta)
+	
 	move_and_slide()
